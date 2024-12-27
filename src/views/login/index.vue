6d22c3212a3ea368d8db406b4bd397baf33d8d14
ZJ <1739593209@qq.com>
2024-12-27 11:13:42 +0800

    登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,72 +1,41 @@
 <template>
   <div class="login-container">
-    <v-card
-      class="mx-auto pa-12 pb-8"
-      elevation="8"
-      max-width="448"
-      rounded="lg"
-    >
-      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+    <v-card class="mx-auto pa-10 pb-8" elevation="8" width="448" rounded="lg">
+      <div class="text-2xl mb-8">线棒材轧制数字化系统</div>
 
       <v-text-field
-        density="compact"
-        placeholder="Email address"
+        label="账号"
+        placeholder="请输入账号"
         prepend-inner-icon="mdi-email-outline"
         variant="outlined"
       ></v-text-field>
 
-      <div
-        class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
-      >
-        Password
-
-        <a
-          class="text-caption text-decoration-none text-blue"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Forgot login password?</a
-        >
-      </div>
-
       <v-text-field
         :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
         :type="visible ? 'text' : 'password'"
-        density="compact"
-        placeholder="Enter your password"
+        label="密码"
+        placeholder="请输入密码"
         prepend-inner-icon="mdi-lock-outline"
         variant="outlined"
         @click:append-inner="visible = !visible"
+        hide-details
       ></v-text-field>
 
-      <v-card class="mb-12" color="surface-variant" variant="tonal">
-        <v-card-text class="text-medium-emphasis text-caption">
-          Warning: After 3 consecutive failed login attempts, you account will
-          be temporarily locked for three hours.
-        </v-card-text>
-      </v-card>
+      <div class="d-flex justify-space-between align-center py-2">
+        <v-checkbox label="记住密码" color="#0c5fff" hide-details class="ml-[-10px]"></v-checkbox>
+        <span>忘记密码</span>
+      </div>
 
       <v-btn
         class="mb-8"
-        color="blue"
+        color="#0c5fff"
         size="large"
         variant="tonal"
         block
         @click="login"
       >
-        Log In
+        登 录
       </v-btn>
-
-      <v-card-text class="text-center">
-        <a
-          class="text-blue text-decoration-none"
-          href="#"
-          rel="noopener noreferrer"
-        >
-          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-        </a>
-      </v-card-text>
     </v-card>
   </div>
 </template>
